b08c8a726d041df8d43a3aee3a8989ed9c17f604
Mohammed Aeraf <aeraf@outlook.com>
2025-06-16 17:10:04 +0530

    JS Intro

---
+++ 01-intro/script.js
@@ -0,0 +1,7 @@
+function changeMessage() {
+  const msg = document.getElementById("message");
+
+  msg.textContent = "Welcome to Frontend React Course";
+  msg.style.color = "red";
+  msg.style.fontWeight = "bold";
+}
