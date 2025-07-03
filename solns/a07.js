ff94eedbe33d4bd9feca46dd7e563d7c2b9ef315
Mohammed Aeraf <aeraf@outlook.com>
2025-07-03 10:29:06 +0530

    a07 soln

---
+++ solns/a07.js
@@ -0,0 +1,8 @@
+const numbers = [4, 7, 12, 15, 9];
+let modifiedNumbers = numbers.map((n) => (n % 2 == 0 ? n * 2 : n + 1));
+console.log(modifiedNumbers);
+
+const numbers2 = [12, 5, 8, 130, 44, 3, 25, 60, 7, 90];
+evenNumbers = numbers2.filter((n) => n % 2 === 0);
+console.log(evenNumbers);
+
