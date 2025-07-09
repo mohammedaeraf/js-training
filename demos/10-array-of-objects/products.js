5e8b95f246e10b455d88fb2c5e90e620fcc70cf1
Mohammed Aeraf <aeraf@outlook.com>
2025-07-09 16:12:28 +0530

    inventory value

--- demos/10-array-of-objects/products.js
+++ demos/10-array-of-objects/products.js
@@ -1,7 +1,7 @@
 let products = [
-  { name: "Laptop", price: 900 },
-  { name: "Mouse", price: 25 },
-  { name: "Monitor", price: 150 },
+  { name: "Laptop", price: 900, stock: 20 },
+  { name: "Mouse", price: 25, stock: 100 },
+  { name: "Monitor", price: 150, stock: 50 },
 ];
 
 // Print name and price of each product
@@ -12,3 +12,10 @@ products.forEach((p) => {
 // Filter products above ₹100
 let expensive = products.filter((p) => p.price > 100);
 console.log("Expensive Products:", expensive);
+
+// Find total value of Inventory in Warehouse/Store
+let inventoryValue = products.reduce(
+  (total, product) => total + (product.price * product.stock),
+  0
+);
+console.log(`Total Value of Inventory in Warehouse = ${inventoryValue}`);
